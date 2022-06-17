b98bf9e78e8798727d1da7dc616627d0254cf94c
yashwant <yashtejwani00@gmail.com>
2022-06-17 19:19:57 +0530

    test api added

--- src/main/java/com/dynamos/aurigabot/controller/Test.java
+++ src/main/java/com/dynamos/aurigabot/controller/Test.java
@@ -1,11 +1,16 @@
 package com.dynamos.aurigabot.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 public class Test {
 
-
+    @PostMapping("/api/v1/test")
+    public String testAPICall(@RequestBody JsonNode json){
+        System.out.println(json);
+        return "test";
+    }
 
 }
