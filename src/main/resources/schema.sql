5bd212a02336cd1549bd96e3663c8b39f10243d7
auriga <sharma.rahul@aurigait.com>
2022-09-17 20:47:25 +0530

    Changes in queries of schema.sql ,Adding dob field in User entity, minor change in WebPortalAdaptor.java

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,58 +1,23 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS users (
-    id uuid uuid_generate_v4 () PRIMARY KEY,
+    id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
     name varchar(100) NOT NULL,
-    mobile varchar(10) NULL,
+    mobile varchar(10) NOT NULL,
     email VARCHAR(255) NOT NULL,
     username varchar(100) NOT NULL,
     employee_id uuid NOT NULL UNIQUE,
+    dob DATE,
     joined_on timestamp without time zone,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-
-
-);
-
-CREATE TABLE IF NOT EXISTS userMessage (
-    id uuid DEFAULT uuid_generate_v4 (),
-
-    command_id uuid FOREIGN KEY REFERENCES commands(id),
-    flow_id uuid FOREIGN KEY REFERENCES flow(id),
-
-    index int NOT NULL,
-    from_user_id uuid FOREIGN KEY REFERENCES users(id),
-    from_source text NOT NULL,
-    to_user_id uuid FOREIGN KEY REFERENCES users(id),
-    to_source text NOT NULL,
-
-    channel varchar(50) NOT NULL,
-    provider varchar(50) NOT NULL,
-    message text NOT NULL,
-    status varchar(20) NOT NULL,
-    received_at timestamp without time zone,
-    sent_at timestamp without time zone,
-    delivered_at timestamp without time zone,
-    read_at timestamp without time zone
-
-
-
-
-
-);
-CREATE TABLE IF NOT EXISTS commands (
-    id uuid  uuid_generate_v4 () PRIMARY KEY,
-    command_type VARCHAR(255) NOT NULL,
-    description text NOT NULL,
-    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 
 
 );
 
 CREATE TABLE IF NOT EXISTS flow(
-    id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    command_id uuid FOREIGN KEY REFERENCES commands(id),
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    command_id uuid REFERENCES commands(id),
     question text NOT NULL,
     index integer NOT NULL,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
@@ -60,20 +25,56 @@ CREATE TABLE IF NOT EXISTS flow(
 
 );
 
+CREATE TABLE IF NOT EXISTS user_message (
+    id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
+
+    command_id uuid REFERENCES commands(id),
+    flow_id uuid REFERENCES flow(id),
+    index int NOT NULL,
+    from_user_id uuid REFERENCES users(id),
+    from_source text NOT NULL,
+    to_user_id uuid REFERENCES users(id),
+    to_source text NOT NULL,
+    channel varchar(50) NOT NULL,
+    provider varchar(50) NOT NULL,
+    message text NOT NULL,
+    status varchar(20) NOT NULL,
+    received_at timestamp without time zone DEFAULT NULL,
+    sent_at timestamp without time zone DEFAULT NULL,
+    delivered_at timestamp without time zone DEFAULT NULL,
+    read_at timestamp without time zone DEFAULT NULL
+
+
+
+
+
+);
+CREATE TABLE IF NOT EXISTS commands (
+    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    command_type VARCHAR(255) NOT NULL,
+    description text NOT NULL,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+
+
+);
+
+
+
 CREATE TABLE IF NOT EXISTS leave_request (
     id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    employee_id uuid FOREIGN KEY REFERENCES users(id),
+    employee_id uuid REFERENCES users(id),
     reason text NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
     status varchar(20)  NOT NULL,
-    approved_by uuid FOREIGN KEY REFERENCES users(id),
+    approved_by uuid REFERENCES users(id),
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS leave_balance (
     id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY,
-    employee_id uuid FOREIGN KEY REFERENCES users(employee_id),
+    employee_id uuid REFERENCES users(employee_id),
     cl integer DEFAULT 0,
     pl integer DEFAULT 0,
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
@@ -82,8 +83,8 @@ CREATE TABLE IF NOT EXISTS leave_balance (
 
 CREATE TABLE IF NOT EXISTS employee_manager(
     id uuid DEFAULT uuid_generate_v4 () PRIMARY KEY ,
-    employee_id uuid FOREIGN KEY REFERENCES users(id),
-    manager_id uuid FOREIGN KEY REFERENCES users(id),
+    employee_id uuid REFERENCES users(id),
+    manager_id uuid REFERENCES users(id),
     created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
