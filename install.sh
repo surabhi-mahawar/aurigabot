2d0bdc0a2b713f4cca7e524345121b1605a3adca
Surabhi <surabhi.mahawar@aurigait.com>
2022-11-16 10:19:39 +0530

    start script

--- install.sh
+++ install.sh
@@ -10,6 +10,28 @@ echo ""
 sed -i "s|TELEGRAM_BOT_USERNAME=.*|TELEGRAM_BOT_USERNAME=${TELEGRAM_BOT_USERNAME}|g" .env
 sed -i "s|TELEGRAM_BOT_TOKEN=.*|TELEGRAM_BOT_TOKEN=${TELEGRAM_BOT_TOKEN}|g" .env
 
-sudo mvn clean install -DskipTests
-sudo docker build -t surabhidocker28/aurigabot:v1 .
-sudo docker-compose up -d aurigabotdb aurigabot
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker already available"
+    echo ""
+else
+    echo "Installing Docker"
+    echo ""
+    #  Install Docker
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    # DRY_RUN=1 sh ./get-docker.sh
+fi
+if [ -x "$(command -v docker)" ]; then
+    echo "Docker Compose already available"
+    echo ""
+else
+    echo "Installing docker-compose"
+    echo ""
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+sudo docker-compose up -d postgresqldb
+mvn clean install -DskipTests
+#mvn spring-boot:run
+docker build -t surabhidocker28/aurigabot .
+docker run --env-file .env --network=aurigabot_mynet -d -p8080:8080 surabhidocker28/aurigabot
